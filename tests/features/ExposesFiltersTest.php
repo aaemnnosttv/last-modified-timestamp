b2eb34568fb6df9282881c1ae751b8627496cba3
Evan Mattson <me@aaemnnost.tv>
2017-03-14 13:38:28 +0200

    update closures for 5.3, cannot use $this

--- tests/features/ExposesFiltersTest.php
+++ tests/features/ExposesFiltersTest.php
@@ -5,17 +5,18 @@ class ExposesFiltersTest extends WP_UnitTestCase
 	/** @test */
 	function allows_defaults_to_be_filtered()
 	{
+		$test_case = $this;
 		$called = false;
 
-		add_filter('last_modified_timestamp_defaults', function ($all_defaults, $context) use (&$called) {
+		add_filter('last_modified_timestamp_defaults', function ($all_defaults, $context) use ($test_case, &$called) {
 			$called = true;
 
-			$this->assertTrue(isset($all_defaults['base']));
-			$this->assertTrue(isset($all_defaults['contexts']['messages']));
-			$this->assertTrue(isset($all_defaults['contexts']['publish-box']));
-			$this->assertTrue(isset($all_defaults['contexts']['shortcode']));
-			$this->assertTrue(isset($all_defaults['contexts']['wp-table']));
-			$this->assertSame('some-context', $context);
+			$test_case->assertTrue(isset($all_defaults['base']));
+			$test_case->assertTrue(isset($all_defaults['contexts']['messages']));
+			$test_case->assertTrue(isset($all_defaults['contexts']['publish-box']));
+			$test_case->assertTrue(isset($all_defaults['contexts']['shortcode']));
+			$test_case->assertTrue(isset($all_defaults['contexts']['wp-table']));
+			$test_case->assertSame('some-context', $context);
 
 			return $all_defaults;
 		}, 10, 2);
@@ -30,13 +31,14 @@ class ExposesFiltersTest extends WP_UnitTestCase
 	/** @test */
 	function allows_output_to_be_filtered()
 	{
+		$test_case = $this;
 		$called = false;
 
-		add_filter('last_modified_timestamp_output', function ($timestamp, $context) use (&$called) {
+		add_filter('last_modified_timestamp_output', function ($timestamp, $context) use ($test_case, &$called) {
 			$called = true;
 
-			$this->assertContains('last-modified-timestamp', $timestamp);
-			$this->assertSame('some-context', $context);
+			$test_case->assertContains('last-modified-timestamp', $timestamp);
+			$test_case->assertSame('some-context', $context);
 
 			return $timestamp;
 		}, 10, 2);
