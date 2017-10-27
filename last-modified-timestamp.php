fb10eac40c35cfe1d5a185d4f78f898fef15b60b
Evan Mattson <me@aaemnnost.tv>
2017-10-27 08:17:04 +0200

    update plugin header format

--- last-modified-timestamp.php
+++ last-modified-timestamp.php
@@ -1,14 +1,14 @@
 <?php
-/*
-	Plugin Name: Last Modified Timestamp
-	Version: 2.0.0-alpha
-	Description: This plugin adds information to the admin interface about when each post/page was last modified (including custom post types!). Use the [last-modified] shortcode in your content!
-	Text Domain: last-modified-timestamp
-	Domain Path: /languages
-	Author: Evan Mattson
-	Author URI: https://aaemnnost.tv/
-	Plugin URI: https://github.com/aaemnnosttv/last-modified-timestamp
-*/
+/**
+ * Plugin Name: Last Modified Timestamp
+ * Version: 2.0.0-alpha
+ * Description: Adds information to the admin interface about when each post/page was last modified (including custom post types!). Use the [last-modified] shortcode in your content!
+ * Text Domain: last-modified-timestamp
+ * Domain Path: /languages
+ * Author: Evan Mattson
+ * Author URI: https://aaemnnost.tv/
+ * Plugin URI: https://github.com/aaemnnosttv/last-modified-timestamp
+ */
 
 define('LAST_MODIFIED_TS__DIR', dirname(__FILE__));
 
