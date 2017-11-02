6af1e2e71ba3d03b8113ff529e359bcb75ad3d19
Evan Mattson <me@aaemnnost.tv>
2017-11-02 10:55:29 +0100

    update tests + formatting

--- tests/features/ProvidesShortcodeTest.php
+++ tests/features/ProvidesShortcodeTest.php
@@ -2,71 +2,96 @@
 
 class ProvidesShortcodeTest extends WP_UnitTestCase
 {
-	/** @test */
-	function shortcode_returns_last_modified_timestamp()
-	{
-		$this->assertShortcodeContains('last-modified-timestamp');
-	}
+    /** @test */
+    function shortcode_returns_last_modified_timestamp()
+    {
+        $this->assertShortcodeContains('last-modified-timestamp');
+    }
 
-	/** @test */
-	function shortcode_can_accept_a_custom_format()
-	{
-		$this->assertShortcodeContains('custom format', array(
-			'format' => 'custom format',
-		));
-	}
+    /** @test */
+    function shortcode_can_accept_a_custom_format()
+    {
+        $this->assertShortcodeContains('custom format', array(
+            'format' => 'custom format',
+        ));
+    }
 
-	/** @test */
-	function shortcode_can_accept_a_custom_date_format()
-	{
-		global $post;
-		$post = $this->factory->post->create_and_get();
-		$expected = (string) get_the_modified_date('U');
+    /** @test */
+    function shortcode_can_accept_a_custom_date_format()
+    {
+        $this->new_global_post();
+        $expected = (string) get_the_modified_date('U');
 
-		$this->assertShortcodeContains($expected, array(
-			'datef' => 'U',
-			'format' => '%date%'
-		));
-	}
+        $this->assertShortcodeContains($expected, array(
+            'datef' => 'U',
+            'format' => '%date%'
+        ));
+    }
 
-	/** @test */
-	function shortcode_can_accept_a_custom_time_format()
-	{
-		global $post;
-		$post = $this->factory->post->create_and_get();
-		$expected = (string) get_the_modified_time('U');
+    /** @test */
+    function shortcode_can_accept_a_custom_time_format()
+    {
+        $this->new_global_post();
+        $expected = (string) get_the_modified_time('U');
 
-		$this->assertShortcodeContains($expected, array(
-			'timef' => 'U',
-			'format' => '%time%'
-		));
-	}
+        $this->assertShortcodeContains($expected, array(
+            'timef' => 'U',
+            'format' => '%time%'
+        ));
+    }
 
-	/** @test */
-	function shortcode_can_accept_a_custom_separator()
-	{
-		$this->assertShortcodeContains(
-			'__START__--separator--__END__',
-			array(
-				'sep' => '--separator--',
-				'format' => '__START__%sep%__END__'
-			)
-		);
-	}
-	
-	/**
-	 * Call the shortcode directly and assert the return value contains a given string.
-	 *
-	 * @param       $string
-	 * @param array $atts
-	 */
-	protected function assertShortcodeContains($string, $atts = array(), $content = '')
-	{
-		global $shortcode_tags;
-		$output = call_user_func($shortcode_tags['last-modified'], $atts, $content, 'last-modified');
+    /** @test */
+    function shortcode_can_accept_a_custom_separator()
+    {
+        $this->assertShortcodeContains(
+            '--separator--',
+            array(
+                'sep' => '--separator--',
+                'format' => '__START__%sep%__END__'
+            )
+        );
+    }
 
-		$this->assertContains($string, $output,
-			'Failed to assert the shortcode output contains the given string.'
-		);
-	}
+    /** @test */
+    function shortcode_can_include_the_author()
+    {
+        $post = $this->new_global_post();
+        $author = $this->factory->user->create_and_get(array('display_name' => 'The Name'));
+        $this->edit_post($post, $author);
+
+        $this->assertShortcodeContains('The Name', array(
+            'format' => '%author%',
+        ));
+    }
+
+    protected function new_global_post($args = array())
+    {
+        $GLOBALS['post'] = $this->factory->post->create_and_get($args);
+
+        return $GLOBALS['post'];
+    }
+
+    protected function edit_post($post, $user = null)
+    {
+        $post_id = get_post_field('ID', $post) ?: $post;
+        $user_id = $user instanceof WP_User ? $user->ID : $user;
+
+        update_post_meta($post_id, '_edit_last', (int) $user_id);
+    }
+
+    /**
+     * Call the shortcode directly and assert the return value contains a given string.
+     *
+     * @param       $string
+     * @param array $atts
+     */
+    protected function assertShortcodeContains($string, $atts = array(), $content = '')
+    {
+        global $shortcode_tags;
+        $output = call_user_func($shortcode_tags['last-modified'], $atts, $content, 'last-modified');
+
+        $this->assertContains($string, $output,
+            'Failed to assert the shortcode output contains the given string.'
+        );
+    }
 }
--- tests/unit/LastModified__ContextTest.php
+++ tests/unit/LastModified__ContextTest.php
@@ -3,36 +3,45 @@
 
 class LastModifiedContextTest extends PHPUnit_Framework_TestCase
 {
-	/** @test */
-	function can_get_a_configuration_value_from_the_context()
-	{
-		$context = new LastModified__Context;
+    /** @test */
+    function can_get_a_configuration_value_from_the_context()
+    {
+        $context = new LastModified__Context;
 
-		$this->assertSame('M j, Y', $context->get('datef'));
-		$this->assertSame('some default', $context->get('non-existent', 'some default'));
-	}
+        $this->assertSame('M j, Y', $context->get('datef'));
+        $this->assertSame('some default', $context->get('non-existent', 'some default'));
+    }
 
-	/** @test */
-	function can_accept_an_array_of_overrides()
-	{
-		$context = new LastModified__Context;
-		$this->assertSame('M j, Y', $context->get('datef'));
+    /** @test */
+    function can_accept_an_array_of_overrides()
+    {
+        $context = new LastModified__Context;
+        $this->assertSame('M j, Y', $context->get('datef'));
 
-		$context->merge(array(
-			'datef' => 'foo'
-		));
+        $context->merge(array(
+            'datef' => 'foo'
+        ));
 
-		$this->assertSame('foo', $context->get('datef'));
-	}
+        $this->assertSame('foo', $context->get('datef'));
+    }
 
-	/** @test */
-	function can_generate_a_timestamp()
-	{
-		$context = new LastModified__Context;
+    /** @test */
+    function can_generate_a_timestamp()
+    {
+        $context = new LastModified__Context;
 
-		$timestamp = $context->render_timestamp();
+        $timestamp = $context->render_timestamp();
 
-		$this->assertNotEmpty($timestamp);
-	}
+        $this->assertNotEmpty($timestamp);
+    }
 
+    /** @test */
+    function contextual_defaults_take_precedence_over_base()
+    {
+        $base_context = new LastModified__Context;
+        $messages_context = new LastModified__Context('messages');
+
+        $this->assertNotContains('%author%', $base_context->get('format'));
+        $this->assertContains('%author%', $messages_context->get('format'));
+    }
 }
