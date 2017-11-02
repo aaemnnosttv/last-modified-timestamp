0210db151c493b10d7db95e9b4575d26f8684a11
Evan Mattson <me@aaemnnost.tv>
2017-11-02 19:06:33 +0100

    rename tests

--- tests/unit/FormattedStringTest.php
+++ tests/unit/FormattedStringTest.php
@@ -2,7 +2,7 @@
 
 use LastModifiedTimestamp\FormattedString;
 
-class LastModifiedFormattedStringTest extends PHPUnit_Framework_TestCase
+class FormattedStringTest extends PHPUnit_Framework_TestCase
 {
 	/** @test */
 	function can_render_placeholders_in_a_template_string_to_values()
