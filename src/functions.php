640ff5b045a365e4341f515feeb65014029d132a
Evan Mattson <me@aaemnnost.tv>
2017-11-02 11:46:52 +0100

    only define functions if not exist

--- src/functions.php
+++ src/functions.php
@@ -1,11 +1,25 @@
 <?php
 
-function get_the_last_modified_timestamp( $context = null, $override = null )
-{
-	return LastModifiedTimestamp::get_instance()->construct_timestamp( $context, $override );
-}
+if ( ! function_exists('get_the_last_modified_timestamp')) :
+    /**
+     * @param null $context
+     * @param null $override
+     *
+     * @return string
+     */
+    function get_the_last_modified_timestamp($context = null, $override = null)
+    {
+        return LastModifiedTimestamp::get_instance()->construct_timestamp($context, $override);
+    }
+endif;
 
-function the_last_modified_timestamp( $context = null, $override = null )
-{
-	echo get_the_last_modified_timestamp( $context, $override );
-}
+if ( ! function_exists('the_last_modified_timestamp')) :
+    /**
+     * @param null $context
+     * @param null $override
+     */
+    function the_last_modified_timestamp($context = null, $override = null)
+    {
+        echo get_the_last_modified_timestamp($context, $override);
+    }
+endif;
